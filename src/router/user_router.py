c54fede9c71f2ad27cf1577e4e844337a4ed8155
vdtheone <vishalprajapati22052001@gmail.com>
2023-09-05 10:22:17 +0530

    add four route create, login, forgot, reset_pass

--- src/router/user_router.py
+++ src/router/user_router.py
@@ -1,17 +1,29 @@
-from flask import Blueprint
-from src.views.user import create_user, test
+from flask import Blueprint, Request
+from src.views.user import create_user, login, forgot, reset_password
 
 
 user_bp = Blueprint('user', __name__)
 
 
-@user_bp.route('/') 
-def index():	
-    test()
-    return "data"	
-
-
 @user_bp.route('/create_user', methods=['POST'])    
-def kfj():	
+def user_create():	
     data = create_user()
+    return data
+
+
+@user_bp.route('/login', methods=['POST'])    
+def login_user():	
+    data = login()
+    return data
+
+
+@user_bp.route('/forgot', methods=['GET'])
+def forgot_pass():
+    data = forgot()
+    return data
+
+
+@user_bp.route('/reset_pass', methods=['PUT'])
+def reset_pass():
+    data = reset_password()
     return data
--- src/views/user.py
+++ src/views/user.py
@@ -1,28 +1,173 @@
-from flask import jsonify, request
+from flask import *
 from src.models.user import User
 from config import SessionLocal
+from hashlib import sha256
+from flask_mail import *  
+from random import *  
+import app
+from src.utils.generate_jwt import create_access_token
+from src.utils.access_token_requied import required_access_token
+import jwt
+import os
+from sqlalchemy.exc import IntegrityError
+
+
+JWT_SECRET_KEY = os.environ.get("JWT_SECRET_KEY")
+ALGORITHM = os.environ.get("ALGORITHM") 
+ 
 
 db = SessionLocal()
 
 
-def test():
-    new_user = User(email='john@example.com', password='vishal')
-    db.add(new_user)
-    db.commit()
+def hash_password(password: str):
+    hash_obj = sha256()
+    hash_obj.update(password.encode("utf-8"))
+    hashed_password = hash_obj.hexdigest()
+    return hashed_password
 
 
 def create_user():
     try:
         data = request.json
-        if "email" in data and "password" in data:
-            email = data["email"]
-            password = data["password"]
-            # Here you can insert the user data into your database or perform other actions
-            new_user = User(email=email, password=password)
-            db.add(new_user)
-            db.commit()
-            return jsonify({"message": "User created successfully"})
-        else:
+
+        if not data:
             return jsonify({"error": "Invalid data format"})
+     
+        otp = randint(000000,999999)
+
+        email = data.get("email")
+        password = data.get("password")
+        
+
+        if not email and not password:
+            return jsonify({"error": "Email, password, or OTP is missing"}), 400
+        
+        # Send OTP via email
+        send_otp_email(email, otp)
+        
+        password = hash_password(password)
+
+        new_user = User(email=email, password=password, otp=otp)
+        db.add(new_user)
+        db.commit()
+        return jsonify({"message": "User created successfully"})
+    
+    except IntegrityError:
+        return jsonify({"error": "Email already exist"})
+
     except Exception as e:
         return jsonify({"error": str(e)})
+    
+# Send OTP via email    
+def send_otp_email(recipient_email, otp):
+    try:
+        msg = Message('OTP', sender='vishal262.rejoice@gmail.com', recipients=[recipient_email])
+        msg.body = str(otp)
+        app.mail.send(msg)
+    except Exception as e:
+        print(f"Error sending OTP email: {str(e)}")
+
+
+def login():
+    try:
+        data = request.json
+        
+        if not data:
+            return jsonify({"error": "Invalid data format"}), 400
+   
+        email = data.get("email")
+        password = data.get("password")
+        password = hash_password(password)
+        otp_return = data.get("otp")
+
+        if not email or not password or not otp_return:
+            return jsonify({"error": "Email, password, or OTP is missing"}), 400
+
+        user_details = db.query(User).filter_by(email=email,password=password,otp=otp_return).first()
+        
+        if user_details is None:
+            return jsonify({"error": "email or passworrd or otp is wrong"}), 404
+        
+        if not user_details.is_varified:
+            user_details.is_varified = True
+            db.commit() 
+            db.refresh(user_details)
+        
+        user_dict = {"id":user_details.id, "email":user_details.email}
+        access_token = create_access_token(user_dict)
+        
+        return jsonify({"message": "Login successful", "token":access_token})
+
+    except Exception as e:
+        return jsonify({"error": str(e)})
+ 
+@required_access_token
+def forgot():
+    try:
+        token = request.headers.get("Authorization").split()
+        if not token:
+            return jsonify({"error": "token not provided"})
+        payload = jwt.decode(token[1], JWT_SECRET_KEY,ALGORITHM)
+
+        user = db.query(User).filter_by(id=payload.get('id')).first()
+        if not user:
+            return jsonify({"error": "user not found"})
+
+        if not user.is_varified:
+            return jsonify({"error": "Login first"})
+        
+        otp = randint(000000,999999)
+
+        send_otp_email(user.email, otp)
+        
+        user.otp = otp
+        db.commit()
+        db.refresh(user)
+
+        return jsonify({"message": "sent otp to your email id"})
+        
+    except Exception as e:
+        return jsonify({"error": str(e)})
+
+
+def get_jwt_identity():
+    try:
+        token = request.headers.get("Authorization").split()
+        if not token:
+            return jsonify({"error": "token not provided"})
+        payload = jwt.decode(token[1], JWT_SECRET_KEY,ALGORITHM)
+        return payload.get('id')
+    except Exception as e:
+        return jsonify({"error":str(e)})
+
+
+@required_access_token
+def reset_password():
+    try:
+        current_user_id = get_jwt_identity()
+        user = db.query(User).filter_by(id=current_user_id).first()
+
+        data = request.json
+        if not data:
+            return jsonify({"error": "Invalid data format"}), 400
+
+        password = data.get('password')
+        otp_return = data.get('otp')
+
+        if not password or not otp_return:
+            return jsonify({"error": "Please provide both password and OTP"}), 400
+
+        if user.otp != otp_return:
+            return jsonify({"error": "Invalid OTP"}), 400
+
+        # Hash the new password
+        hashed_password = hash_password(password)
+        user.password = hashed_password
+
+        db.commit()
+        db.refresh(user)
+
+        return jsonify({"message": "Password reset successfully"})
+
+    except Exception as e:
+        return jsonify({"error": "An error occurred while resetting the password"}), 500
