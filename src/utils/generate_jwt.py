9ad8f23bb190a5c0e46a2f322e59fcdead87f07b
vdtheone <vishalprajapati22052001@gmail.com>
2023-09-05 10:16:41 +0530

    jwt token

---
+++ src/utils/generate_jwt.py
@@ -0,0 +1,14 @@
+from datetime import datetime, timedelta
+import os
+
+import jwt
+
+JWT_SECRET_KEY = os.environ.get("JWT_SECRET_KEY")
+ALGORITHM = os.environ.get("ALGORITHM") 
+
+def create_access_token(payload):
+    expire = datetime.utcnow() + timedelta(minutes=180)
+    payload.update({"exp": expire})
+    encoded_token = jwt.encode(payload, JWT_SECRET_KEY,ALGORITHM)
+    return f"Bearer {encoded_token}"
+
